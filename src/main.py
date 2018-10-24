0d2aae90cda73b341863a4e602edd4faab8b2c65
Ruggero Turra <ruggero.turra@cern.ch>
2018-10-24 23:45:05 +0200

    pep8, cleaning

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import exifread
 import os
-from dateutil import parser
 from datetime import datetime
 import shutil
 import filecmp
@@ -17,23 +16,27 @@ ch.setLevel(logging.DEBUG)
 
 logger.addHandler(ch)
 
+
 def match_filename(filename):
     return '.jpg' in filename or '.JPG' in filename
+
 
 def filename_generator_python2(directory_name):
     import os
 
-    matches = []
     for root, dirnames, filenames in os.walk(directory_name):
         for filename in filenames:
             yield os.path.join(root, filename)
 
+
 def filename_generator_python3(directory_name):
     import glob
+
     def either(c):
-        return '[%s%s]' % (c.lower(),c.upper()) if c.isalpha() else c
+        return '[%s%s]' % (c.lower(), c.upper()) if c.isalpha() else c
     for filename in glob.iglob('{0}/**/*.{1}'.format(directory_name, 'jpg'), recursive=True):
         yield filename
+
 
 def filename_generator(directory_name):
     import sys
@@ -41,7 +44,8 @@ def filename_generator(directory_name):
         return filename_generator_python3(directory_name)
     else:
         return filename_generator_python2(directory_name)
-    
+
+
 def get_filename_date(filename):
     with open(filename, 'rb') as f:
         exif_data = exifread.process_file(f, stop_tag='EXIF DateTimeOriginal')
@@ -49,13 +53,16 @@ def get_filename_date(filename):
         if date:
             return date.values
 
+
 def parse_date(date_string):
     if not date_string:
         return None
     return datetime.strptime(date_string, "%Y:%m:%d %H:%M:%S")
 
+
 def get_new_path(basedir, date):
     return os.path.join(basedir, '{:04d}/{:02d}/{:02d}'.format(date.year, date.month, date.day))
+
 
 def copy_image(source, dest_dir, overwrite=False, remove_source=False):
     remove = os.remove if remove_source else (lambda x: None)
@@ -78,6 +85,7 @@ def copy_image(source, dest_dir, overwrite=False, remove_source=False):
         shutil.copy2(source, dest)
         remove(source)
 
+
 if __name__ == "__main__":
     import argparse
 
@@ -91,11 +99,10 @@ if __name__ == "__main__":
     input_directory = args.input
     output_directory = args.output
     for fn in filename_generator(input_directory):
-        date  = get_filename_date(fn)
+        date = get_filename_date(fn)
         date = parse_date(date)
         if date:
             new_path = get_new_path(output_directory, date)
             copy_image(fn, new_path, overwrite=args.overwrite, remove_source=args.remove_source)
         else:
             logger.info("no date for file %s", fn)
-
