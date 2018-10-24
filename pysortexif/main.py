65e99613c85a179be3b0d26a2eac898937c3eecb
Ruggero Turra <ruggero.turra@cern.ch>
2018-10-25 00:33:46 +0200

    black style code

--- pysortexif/main.py
+++ pysortexif/main.py
@@ -7,7 +7,7 @@ import filecmp
 import logging
 
 # create logger
-logger = logging.getLogger('logger')
+logger = logging.getLogger("logger")
 logger.setLevel(logging.DEBUG)
 
 # create console handler and set level to debug
@@ -18,7 +18,7 @@ logger.addHandler(ch)
 
 
 def match_filename(filename):
-    return '.jpg' in filename or '.JPG' in filename
+    return ".jpg" in filename or ".JPG" in filename
 
 
 def filename_generator_python2(directory_name):
@@ -33,13 +33,17 @@ def filename_generator_python3(directory_name):
     import glob
 
     def either(c):
-        return '[%s%s]' % (c.lower(), c.upper()) if c.isalpha() else c
-    for filename in glob.iglob('{0}/**/*.{1}'.format(directory_name, 'jpg'), recursive=True):
+        return "[%s%s]" % (c.lower(), c.upper()) if c.isalpha() else c
+
+    for filename in glob.iglob(
+        "{0}/**/*.{1}".format(directory_name, "jpg"), recursive=True
+    ):
         yield filename
 
 
 def filename_generator(directory_name):
     import sys
+
     if sys.version_info >= (3, 5):
         return filename_generator_python3(directory_name)
     else:
@@ -47,9 +51,9 @@ def filename_generator(directory_name):
 
 
 def get_filename_date(filename):
-    with open(filename, 'rb') as f:
-        exif_data = exifread.process_file(f, stop_tag='EXIF DateTimeOriginal')
-        date = exif_data.get('EXIF DateTimeOriginal', None)
+    with open(filename, "rb") as f:
+        exif_data = exifread.process_file(f, stop_tag="EXIF DateTimeOriginal")
+        date = exif_data.get("EXIF DateTimeOriginal", None)
         if date:
             return date.values
 
@@ -61,7 +65,9 @@ def parse_date(date_string):
 
 
 def get_new_path(basedir, date):
-    return os.path.join(basedir, '{:04d}/{:02d}/{:02d}'.format(date.year, date.month, date.day))
+    return os.path.join(
+        basedir, "{:04d}/{:02d}/{:02d}".format(date.year, date.month, date.day)
+    )
 
 
 def copy_image(source, dest_dir, overwrite=False, remove_source=False):
@@ -79,7 +85,10 @@ def copy_image(source, dest_dir, overwrite=False, remove_source=False):
                 shutil.copy2(source, dest)
                 remove(source)
             else:
-                logger.warning("filename %s already exists and it is different, no copy is done", dest)
+                logger.warning(
+                    "filename %s already exists and it is different, no copy is done",
+                    dest,
+                )
     else:
         logger.info("moving file %s to %s", fn, new_path)
         shutil.copy2(source, dest)
@@ -89,11 +98,13 @@ def copy_image(source, dest_dir, overwrite=False, remove_source=False):
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description='Organize images in forders by day')
-    parser.add_argument('input', help='input directory')
-    parser.add_argument('output', help='output directory')
-    parser.add_argument('--overwrite', action='store_true', help='overwrite if same image exists')
-    parser.add_argument('--remove-source', action='store_true', help='remove source')
+    parser = argparse.ArgumentParser(description="Organize images in forders by day")
+    parser.add_argument("input", help="input directory")
+    parser.add_argument("output", help="output directory")
+    parser.add_argument(
+        "--overwrite", action="store_true", help="overwrite if same image exists"
+    )
+    parser.add_argument("--remove-source", action="store_true", help="remove source")
     args = parser.parse_args()
 
     input_directory = args.input
@@ -103,6 +114,8 @@ if __name__ == "__main__":
         date = parse_date(date)
         if date:
             new_path = get_new_path(output_directory, date)
-            copy_image(fn, new_path, overwrite=args.overwrite, remove_source=args.remove_source)
+            copy_image(
+                fn, new_path, overwrite=args.overwrite, remove_source=args.remove_source
+            )
         else:
             logger.info("no date for file %s", fn)
