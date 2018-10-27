ccd783b48487490b9da544a318fbbe13c227a930
Ruggero Turra <ruggero.turra@cern.ch>
2018-10-27 19:39:03 +0200

    module structure

---
+++ pysortexif/__init__.py
@@ -0,0 +1,1 @@
+from .pysortexif import parse_date
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,5 +1,6 @@
 import pytest
 import tempfile
+import pysortexif
 import os
 
 @pytest.fixture()
@@ -12,3 +13,7 @@ def cleandir():
 class TestDirectoryInit(object):
     def test_cwd_starts_empty(self):
         assert 1
+
+
+def test_paringdata():
+    assert pysortexif.pysortexif.parse_date("") is None
